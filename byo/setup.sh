8e49f8232b533c11d964a9ac4fa02a38dfa82bbc
Tomas <tomas@sensaway.co.nz>
2017-12-20 11:09:18 +1300

    Changed SSH tests

--- byo/setup.sh
+++ byo/setup.sh
@@ -18,13 +18,11 @@ echo "Type the controller node hostname:"
 read CONTROLLER_NODE_NAME
 echo "Type the controller node IP:"
 read IPCTRL1
-echo "Type the first worker node IP (these need to be continuos for obvious reasons):"
-read IPWRK1
 # User below might need to be changed depending on your needs.
 export SSH_KEYFILE=${SSH_KEYFILE:-${HOME}/.ssh/id_rsa}
 export WORKERS=${WORKERS:-2}
 export VM_USER=${VM_USER:-root}
-export CONTROLLER_NODE_NAME IPCTRL1 IPWRK1
+export CONTROLLER_NODE_NAME IPCTRL1
 
 if [[ ! -f "$SSH_KEYFILE" ]]; then
 	echo >&2 "SSH_KEYFILE $SSH_KEYFILE does not exist."
@@ -38,11 +36,10 @@ echo "Testing SSH connectivity"
 ssh -q -o BatchMode=yes -o ConnectTimeout=10 ${VM_USER}@${CONTROLLER_NODE_NAME} "touch /tmp/byo.node" | exit && echo $host "$CONTROLLER_NODE_NAME: SSH Connection...OK" || echo $host "$CONTROLLER_NODE_NAME: SSH Connection...FAILED" | exit 1
 ssh -q -o BatchMode=yes -o ConnectTimeout=10 ${VM_USER}@${IPCTRL1} exit && echo $host "$IPCTRL1: SSH Connection...OK" || echo $host "$IPCTRL1: SSH Connection...FAILED" | exit 1
 for i in $(seq 0 "$WORKERS"); do
-	worker_node_name="worker-node-${i}"
-        ssh -q -o BatchMode=yes -o ConnectTimeout=10 ${VM_USER}@${worker_node_name} /tmp/byo.node | exit && echo $host "$worker_node_name: SSH Connection...OK" || echo $host "$worker_node_name: SSH Connection...FAILED" | exit 1
-        shopt -s lastpipe
-	dig ${worker_node_name} A +short | tail -n1 | readarray -t theip; ssh -q ${VM_USER}@${theip} exit && echo $host "$theip: SSH Connection...OK" || echo $host "$theip: SSH Connection...FAILED" | exit 1
-        shopt -u lastpipe
+        worker_node_name="worker-node-${i}"
+        ssh -q ${VM_USER}@${worker_node_name} exit && echo $host "$worker_node_name: SSH Connection...OK" || echo $host "$worker_node_name: SSH Connection...FAILED" | exit 1
+        theip=$(host ${worker_node_name} | awk '/has address/ { print $4 }')
+        ssh -q ${VM_USER}@${theip} exit && echo $host "$theip: SSH Connection...OK" || echo $host "$theip: SSH Connection...FAILED" | exit 1
 done
 
 "${SCRIPT_DIR}/provision.sh"
